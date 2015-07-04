5d92970fe02c41a500b26c989405a51a3a41d7e2
tkmru <i.am.tkmru@gmail.com>
2015-07-04 23:26:44 +0900

    add menubar

--- biwx.py
+++ biwx.py
@@ -11,5 +11,33 @@ __license__ = 'MIT License'
 
 import wx
 
+class MainWindow(wx.Frame):
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(500, 500))
+        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.CreateStatusBar() # A Statusbar in the bottom of the window
+
+        # Setting up the menu.
+        file_menu = wx.Menu()
+
+        file_menu.Append(wx.ID_ABOUT, '&About', 'Information about this program')
+        file_menu.Append(wx.ID_PREFERENCES, "&Preferences")
+        file_menu.Append(wx.ID_EXIT,'&Exit', 'Terminate the program')
+
+        file_menu.Append(-1, '&New File')
+        file_menu.Append(-1, '&Open')
+        file_menu.Append(-1, '&Save')
+
+        # Creating the menubar.
+        menu_bar = wx.MenuBar()
+        menu_bar.Append(file_menu, "&File")
+        self.SetMenuBar(menu_bar)  # Adding the MenuBar to the Frame content.
+        self.Show(True)
+
+
+if __name__ == '__main__':
+    app = wx.App(False)
+    frame = MainWindow(None, 'biwx')
+    app.MainLoop()
 
 
